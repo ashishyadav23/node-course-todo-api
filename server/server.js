80e83e313d871bb9fdb4ca50391273e4f5cc9cd7
Ashish <ashish.yadav@cmss.in>
2018-01-31 18:45:57 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -20,6 +20,16 @@ app.post('/todos', (req, res) => {
         console.log(`Erro while saving todos ${req.body.text}`, err);
         res.status(400).send(err);
     });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        })
+    }, (err) => {
+        res.status(400).send(err);
+    })
 })
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
