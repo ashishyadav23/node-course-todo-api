b0565b7d6f201e170094446b2cb7cac97fdd042a
Ashish <ashish.yadav@cmss.in>
2018-02-01 19:15:12 +0530

    setup app fro heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 // mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/MongooseTodoApp');
+var url = {
+    "localhost": "mongodb://localhost:27017/MongooseTodoApp",
+    "Mlab": "mongodb://<ashishyadav23>:<ashish23>@ds221148.mlab.com:21148/ashishtodo"
+}
+var URL =
+    mongoose.connect(url.Mlab||url.localhost);
 module.exports = {
     mongoose
 }
