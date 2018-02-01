f8025981b3883641af25fb1a056005efeba7244f
Ashish <ashish.yadav@cmss.in>
2018-02-01 19:25:47 +0530

    changed mongodb url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // mongoose.Promise = global.Promise;
 var url = {
     "localhost": "mongodb://localhost:27017/MongooseTodoApp",
-    "Mlab": "mongodb://<ashishyadav23>:<ashish23>@ds221148.mlab.com:21148/ashishtodo"
+    "Mlab": "mongodb://ashishyadav23:ashish23@ds221148.mlab.com:21148/ashishtodo"
 }
 var URL =
     mongoose.connect(url.Mlab||url.localhost);
