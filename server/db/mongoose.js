9711180a34ddd4e08b39c527713760818bdfe71d
Ashish <ashish.yadav@cmss.in>
2018-02-02 10:07:02 +0530

    changed mLab username and password

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 // mongoose.Promise = global.Promise;
 var url = {
     "localhost": "mongodb://localhost:27017/MongooseTodoApp",
-    "Mlab": "mongodb://ashishyadav23:ashish23@ds221148.mlab.com:21148/ashishtodo"
+    "Mlab": "mongodb://ashish:ashish@ds221148.mlab.com:21148/ashishtodo"
 }
 var URL =
-    mongoose.connect(url.Mlab||url.localhost);
+    mongoose.connect(url.Mlab);
 module.exports = {
     mongoose
 }
