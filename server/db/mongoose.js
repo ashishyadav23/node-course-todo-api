290da37069309697f10f81c13054b7b101de42eb
Ashish <ashish.yadav@cmss.in>
2018-02-02 10:18:06 +0530

    add HEroku config :set mlab url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 // mongoose.Promise = global.Promise;
-var url = {
-    "localhost": "mongodb://localhost:27017/MongooseTodoApp",
-    "Mlab": "mongodb://ashish:ashish@ds221148.mlab.com:21148/ashishtodo"
-}
+// var url = {
+//     "localhost": "mongodb://localhost:27017/MongooseTodoApp",
+//     "Mlab": "mongodb://ashish:ashish@ds221148.mlab.com:21148/ashishtodo"
+// }
 var URL =
-    mongoose.connect(url.Mlab);
+    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MongooseTodoApp');
 module.exports = {
     mongoose
 }
