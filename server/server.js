ef516937212aac7c5ab68f768379224fdd8aafc2
Ashish <ashish.yadav@cmss.in>
2018-02-05 11:54:09 +0530

    ADD GET /users/me and middleware authenticate

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const _ = require("lodash");
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -112,11 +113,17 @@ app.post('/users', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     }).then((token) => {
-        res.header('x-auth',token).send(user);
+        res.header('x-auth', token).send(user);
     })
         .catch((err) => {
             res.status(400).send(err);
         })
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 })
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
